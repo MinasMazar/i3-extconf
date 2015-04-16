2c7ce711e0477807c9764454f0bb800c287a677a
MinasMazar <minasmazar@gmail.com>
2015-04-16 19:27:18 +0200

    Add littletry, JRuby System Tray Applcation. **WIP**

---
+++ littletry.rb
@@ -0,0 +1,61 @@
+
+#-----------------------------------------------------------------------------
+# Summary: Runs a system tray application with ability to take a screenshot
+#-----------------------------------------------------------------------------
+
+# Import the Java class libraries we wish to use
+include Java
+import java.awt.TrayIcon
+import java.awt.Toolkit
+
+class TrayApp
+  def self.create_item(title)
+    java.awt.MenuItem.new(title)
+  end
+  def self.get_image(path)
+    java.awt.Toolkit::default_toolkit.get_image(path)
+  end
+  def self.create_icon(caption, image, popup)
+    tray_icon = TrayIcon.new(image, caption, popup)
+    tray_icon.image_auto_size = true
+    tray_icon
+  end
+
+  def initialize(caption, image_icon_path)
+    @popup_items = []
+    @caption = caption
+    @image_icon = TrayApp.get_image image_icon_path
+  end
+
+  def append_item(title, type = :normal, &action)
+    item = TrayApp.create_item title
+    if type == :exit
+      item.add_action_listener { java.lang.System::exit(0) }
+    elsif type == :normal
+      item.add_action_listener &action
+    end
+    @popup_items << item
+  end
+
+  def tray_it!
+    tray_icon = TrayApp.create_icon @caption, @image_icon, create_popup
+    @tray = java.awt.SystemTray::system_tray
+    @tray.add(tray_icon)
+  end
+
+  private
+  def create_popup
+    popup = java.awt.PopupMenu.new
+    @popup_items.each do |m|
+      popup.add m
+    end
+    popup
+  end
+end
+
+tray_app = TrayApp.new('Little JRuby Tray Application', './p1010296a.git')
+tray_app.append_item('Exit', :exit) {}
+tray_app.append_item( 'Action1') { system 'x-terminal-emulator' }
+tray_app.append_item('About..') { }
+
+tray_app.tray_it!
