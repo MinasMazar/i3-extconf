f3dd2a939f01bf745246cc3d11193a7dbb7e39d5
MinasMazar <minasmazar@gmail.com>
2015-04-28 03:27:37 +0200

    Implement Outpad feature. Refact I3API

--- i3-ctl.rb
+++ i3-ctl.rb
@@ -2,6 +2,7 @@
 
 require 'json'
 require 'thor'
+require 'pry'
 
 module I3
   module API
@@ -15,7 +16,15 @@ module I3
     def current_workspace
       get_workspaces.find {|w| w["focused"] == true }
     end
-  
+
+    def current_workspaces
+      get_outputs.select {|o| o["current_workspace"] != nil }
+    end
+
+    def get_active_outputs
+      get_outputs.select {|o| o["active"] == true }
+    end
+
     def i3cmd(cmd)
       i3send cmd
     end
@@ -24,10 +33,9 @@ module I3
   
     def i3send(*msg)
       msg = msg.join(", ")
-      print "i3) sending #{msg} => "
-      ret = `i3-msg #{msg}`
-      puts ret.inspect
-      JSON.parse ret
+      ret = JSON.parse `i3-msg #{msg}`
+      puts "i3 send :: [ #{msg} ] => #{ret}"
+      ret
     end
   
   end
@@ -36,10 +44,30 @@ module I3
 
     include API
 
+    def goto_workspace(ws)
+      i3cmd "workspace #{ws}"
+    end
+
+    def goto_output(out)
+      i3cmd "focus output #{out}"
+    end
+
+    def goto_and_hold_workspace(ws = nil)
+      goto_workspace ws if ws
+      i3cmd "exec xmessage \"WORKSPACE-HOLDER\""
+    end
+
     def spiral(args)
       dir = 'h'
       #args.map {|a| [ a, "split #{dir = dir == 'v' ? 'h' : 'v'}" ] }.flatten
       args.flatten.inject([]) {|arr, a| arr + [ a, "split #{dir = dir == 'v' ? 'h' : 'v' }"] }
+    end
+
+    def goto_outpads
+      get_active_outputs.each do |output|
+        goto_output output["name"]
+        goto_workspace "#{output["name"]}:OUTPAD"
+      end
     end
   
     def gotow_and_start_nterms(workspace, nterms)
@@ -53,6 +81,14 @@ module I3
 
 end
 
+class SubcommandExampleCLI < Thor
+
+  desc "subcommand", "subcommand action"
+  def subcommand
+    puts "#{self.class}##{__method__}"
+  end
+end
+  
 class I3CLI < Thor
   include I3::Macros
 
@@ -60,7 +96,7 @@ class I3CLI < Thor
   option :workspace, :default => 8
   option :terms, :default => 3
   def massterm
-    gotow_and_start_nterms(options[:workspace], options[:terms])
+    i3cmd ["exec x-terminal-emulator", "exec x-terminal-emulator", "split v", "exec x-terminal-emulator", "split h" ]
   end
 
   desc "phalanx", "Start phalanx-prybot sessions."
@@ -74,8 +110,21 @@ class I3CLI < Thor
   end
 
   desc "bells", "Bells of Pescolanciano give us the current time."
-  def bells
-    system "rvm default exec pescobells campanaro"
+  def bells(action)
+    system "rvm default exec pescobells #{action}"
+  end
+
+  desc "subcommand", "Thor CLI: Subcommand example"
+  subcommand "subcommand", SubcommandExampleCLI
+
+  desc "outpad", "Escape current workspaces for all outputs"
+  def outpad
+    goto_outpads
+  end
+
+  desc "pry", "Start PRY session inside CLI"
+  def pry
+    binding.pry
   end
 
 end
