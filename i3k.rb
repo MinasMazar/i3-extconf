3023a3b301b2d5c25f2a91addc42fc7aef9d0b87
minasmazar <minasmazar@gmail.com>
2015-08-25 00:47:22 +0200

    BUGFIX: i3 can exec \`commands\` !! i3k.rb from scratch

--- i3k.rb
+++ i3k.rb
@@ -1,59 +1,28 @@
 #!/bin/ruby
 
 require 'i3rb'
-require 'rerun'
 
-module I3::Macros
+class I3::Kommando
 
   include I3::API
-  include I3::CLI
-  include I3::DMenu
 
-  attr_accessor :repl_mode
-  def repl_mode?
-    @repl_mode
+  @@main = I3::DMenu.get_instance
+  @@main.lines = 10
+  @@main.position = :bottom
+
+  def initialize
+    @modes = { :main => @@main }
+    @cur = @modes[:main]
   end
 
-  def repl_mode
-    @repl_mode = true
-  end
-
-  def pop_mode
-    @repl_mode = false
-  end
-
-  def dmenu_loop
-    @entries = []
+  def run
     loop do
-      cmd = get_string.chomp
-      $logger.debug "DMENU: #{cmd}"
-      break if cmd == '' || cmd =~ /quit|exit/
-      ret = self.send cmd
-      if ret != "" && repl_mode?
-        @entries = [ ret ]
-      else
-        @entries << ret
-      end
-      sleep 0.01
+      dmenu_ret = @cur.run
+      break if dmenu_ret.value == 'break'
+      puts self.instance_eval dmenu_ret.value
     end
   end
-
-  def goto_outpad
-    goto_workspace "OUTPAD"
-  end
-
-  def get_workspace_names
-    get_workspaces.map {|w| w["name"] }
-  end
-
-  def wallpaper_rerun
-    Kernel.fork do
-      system "rvm default do rerun -sb -p wallpaper \"feh --bg-scale ~/.i3/wallpaper\" "
-    end
-  end
-
+    
 end
 
-include I3::Macros
-
-run
+I3::Kommando.new.run
