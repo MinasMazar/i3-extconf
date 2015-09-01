987fdeb7af1e61204e01a5c0e6c3dc0da477b892
minasmazar <minasmazar@gmail.com>
2015-09-02 01:24:05 +0200

    Implement script *graffiti*: wallpaper switcher mechanism

---
+++ graffiti.rb
@@ -0,0 +1,7 @@
+
+#exit(-1) if ARGV.empty?
+
+imgs = Dir[File.expand_path("../../Immagini/", __FILE__)+"/*.jpg"]
+rand_wallpaper = imgs.sample
+system "ln -sf #{rand_wallpaper} wallpaper_graffiti"
+system "feh --bg-scale ~/.i3/wallpaper_graffiti"
